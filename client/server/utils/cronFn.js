dee9117b48196f0f7c85de58534025a03d7c280a
davitti <abuadonald@gmail.com>
2022-11-06 02:09:32 +0000

    batch process function

---
+++ client/server/utils/cronFn.js
@@ -0,0 +1,116 @@
+const axios = require("axios");
+const { join } = require("path");
+const { readFileSync, writeFileSync } = require("fs");
+const sha256 = require("js-sha256");
+const path = require("path");
+const signature = sha256(
+  `${process.env.API_KEY}${process.env.API_SECRET}${Math.floor(
+    Date.now() / 1000
+  )}`
+);
+
+// Axios Instance: Partner API
+const partnerAPI = axios.create({
+  baseURL: process.env.API_URL,
+  headers: {
+    "api-key": process.env.API_KEY,
+    "x-signature": signature,
+  },
+});
+
+// Hotels
+async function batch(fetchCycle, urlObj, model) {
+  while (!fetchCycle.done) {
+    if (fetchCycle.count === fetchCycle.cycle) {
+      fetchCycle.done = true;
+      console.log("Done");
+      break;
+    }
+    // Create log file if it doesn't exist
+    try {
+      readFileSync(join(__dirname, "lastUpdateTime.json"));
+    } catch (_) {
+      writeFileSync(join(__dirname, "lastUpdateTime.json"), '{"lastUpdate":0}');
+    }
+    // Last Update
+    const { lastUpdate } = JSON.parse(
+      readFileSync(join(__dirname, "lastUpdateTime.json"), "utf-8")
+    );
+
+    const date = `${new Date(lastUpdate).getFullYear()}-${new Date(
+      lastUpdate
+    ).getMonth()}-${`${new Date(lastUpdate).getDay()}`.padStart(2, "0")}`;
+
+    console.log(urlObj.query.from, urlObj.query.to, lastUpdate);
+
+    fetchCycle.iCount = fetchCycle.count + 1;
+
+    //fetch code here
+    const response = await partnerAPI.get(
+      `${urlObj.url}${urlObj.query && "?"}${
+        urlObj.query.from && `&from=${urlObj.query.from}`
+      }${urlObj.query.to && `&to=${urlObj.query.to}`}${
+        lastUpdate ? `&lastUpdateTime=${date}` : ""
+      }`
+    );
+
+    function resKeys() {
+      return Object.keys(response.data);
+    }
+    console.log("endpoint: ", resKeys().at(-1), path.sep);
+    for (const data of response.data[resKeys().at(-1)])
+      await model.findOneAndUpdate(data, data, { upsert: true });
+
+    urlObj.query.from = urlObj.query.to + 1;
+    urlObj.query.to += urlObj.interval;
+  }
+}
+class FetchCycle {
+  /**
+   * @param {number} cycle
+   */
+  constructor(cycle) {
+    (this.count = 0), (this.cycle = cycle), (this.isDone = false);
+  }
+  /**
+   * @param {number} count
+   * @param {Boolean} boolean
+   */
+  set iCount(count) {
+    this.count = count;
+  }
+  /**
+   * @param {Boolean} boolean
+   */
+  set done(boolean) {
+    this.isDone = boolean;
+  }
+}
+
+class Url {
+  /**
+   * @param {string} url
+   * @param {number} from
+   * @param {number} to
+   */
+  constructor(url, from, to) {
+    (this.url = url),
+      (this.query = {
+        from,
+        to,
+      }),
+      (this.interval = to);
+  }
+  /**
+   * @param {number} int
+   */
+  set iInterval(int) {
+    this.interval = int;
+  }
+}
+
+module.exports = {
+  batch,
+  FetchCycle,
+  Url,
+};
