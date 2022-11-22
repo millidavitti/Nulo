db736bbab76bf41bd4fabe9a74971e41e2144239
davitti <abuadonald@gmail.com>
2022-11-22 12:10:14 +0000

    Update 5 files

--- server/app.js
+++ server/app.js
@@ -2,10 +2,9 @@ const express = require("express");
 const batchProcess = require("./utils/batch.process");
 const app = express();
 
-app.get("/",  (_, res) => {
-   batchProcess();
+app.get("/", (_, res) => {
+  batchProcess();
   res.json({ status: "Batching in progress" });
 });
 
 module.exports = app;
-
