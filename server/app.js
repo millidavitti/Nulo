2d420173057dfbacdd3e28faff409bd4fb39f27c
davitti <abuadonald@gmail.com>
2022-11-22 12:56:18 +0000

    Update 4 files

--- server/app.js
+++ server/app.js
@@ -1,13 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const batchProcess = require("./utils/batch.process");
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 app.get("/", (_, res) => {
   batchProcess();
   res.json({ status: "Batching in progress" });
